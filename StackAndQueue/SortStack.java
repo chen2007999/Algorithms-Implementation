af1a89810dc618fb13b86746865872829e570245
Jiaqi Chen <chen2007999@126.com>
2015-08-09 16:45:54 +0100

    SortStack Algorithm

---
+++ StackAndQueue/SortStack.java
@@ -0,0 +1,19 @@
+package StackAndQueue;
+
+import java.util.Stack;
+
+public class SortStack {
+
+	Stack<Integer> sortStack(Stack<Integer> stack1) {
+		Stack<Integer> stack2 = new Stack<Integer>();
+		while (!stack1.isEmpty()) {
+			int cur = stack1.pop();
+			while (!stack2.isEmpty() && cur > stack2.peek()) {
+				stack1.push(stack2.pop());
+			}
+			stack2.push(cur);
+		}
+		return stack2;
+	}
+
+}
