e61ffaaa0ec4eda65b2650b7e72542e83b551295
Jiaqi Chen <chen2007999@126.com>
2015-08-14 19:05:05 +0100

    update the DFS way of the algorithm

--- TreeAndGraph/ValidateBST.java
+++ TreeAndGraph/ValidateBST.java
@@ -1,45 +1,38 @@
 package TreeAndGraph;
 
 public class ValidateBST {
-	
+
+	Integer last = null;
+
 	boolean validateBST1(TreeNode root) {
-		int result =validateBST1Helper(root);
-		return result != -1;
+		if (root == null)
+			return true;
+
+		if (!validateBST1(root.left))
+			return false;
+
+		if (last != null && root.data > last)
+			return false;
+
+		last = root.data;
+
+		if (!validateBST1(root.right))
+			return false;
+		return true;
 	}
-	
-	int validateBST1Helper(TreeNode root) {
-		if(root == null) return 1;
-		if(root.left != null) {
-			if(validateBST1Helper(root.left) == -1) {
-				return -1;
-			}
-			if(root.left.data > root.data) {
-				return -1;
-			}
-		}
-		if(root.right != null) {
-			if(validateBST1Helper(root.right) == -1) {
-				return -1;
-			}
-			if(root.right.data <= root.data) {
-				return -1;
-			}
-		}
-		return 1;
-	}
-	
+
 	boolean validateBST2(TreeNode root) {
 		return validateBST2Helper(root, null, null);
 	}
 
 	private boolean validateBST2Helper(TreeNode root, Integer min, Integer max) {
-		if(root == null) {
+		if (root == null) {
 			return true;
 		}
-		if((min != null && root.data <= min) || (max != null && root.data > max)) {
+		if ((min != null && root.data <= min) || (max != null && root.data > max)) {
 			return false;
 		}
-		if(!validateBST2Helper(root.left, min, root.data) || !validateBST2Helper(root.right, root.data, max)) {
+		if (!validateBST2Helper(root.left, min, root.data) || !validateBST2Helper(root.right, root.data, max)) {
 			return false;
 		}
 		return true;
