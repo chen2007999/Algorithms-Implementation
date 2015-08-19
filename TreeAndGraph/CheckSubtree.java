91ec0471acf9e5d09e4fce420fae962a29578b82
Jiaqi Chen <chen2007999@126.com>
2015-08-19 23:27:52 +0100

    Fix CheckSubtree Algorithm

--- TreeAndGraph/CheckSubtree.java
+++ TreeAndGraph/CheckSubtree.java
@@ -7,13 +7,13 @@ public class CheckSubtree {
 			return true;
 		if (t1 == null)
 			return false;
-
-		if (t1.left != null) {
-			checkSubtree(t1.left, t2);
-		}
+		
 		if (t1 == t2) {
 			boolean identical = compareTwoTrees(t1, t2);
 			return identical ? true : checkSubtree(t1.left, t2);
+		}
+		if (t1.left != null) {
+			checkSubtree(t1.left, t2);
 		}
 		if (t1.right != null) {
 			checkSubtree(t1.right, t2);
