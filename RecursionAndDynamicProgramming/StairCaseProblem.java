16db436c6c2c5ab0cf9080a51391a3b1965f93a5
Jiaqi Chen <chen2007999@126.com>
2015-08-23 12:53:08 +0100

    StairCaseProblem Algorithm

--- RecursionAndDynamicProgramming/StairCaseProblem.java
+++ RecursionAndDynamicProgramming/StairCaseProblem.java
@@ -1,4 +1,6 @@
-package DynamicProgramming;
+package RecursionAndDynamicProgramming;
+
+import java.util.HashMap;
 
 public class StairCaseProblem {
 
@@ -9,10 +11,10 @@ public class StairCaseProblem {
 	 */
 
 	// bottom up:
-	int numOfWays(int n) { 
+	int numOfWays1(int n) {
 		if (n == 1)
 			return 1;
-		int[] result = new int[n + 1]; 				
+		int[] result = new int[n + 1];
 		result[0] = 1;
 		result[1] = 1;
 		for (int i = 2; i < n; i++) {
@@ -20,22 +22,22 @@ public class StairCaseProblem {
 		}
 		return result[n];
 	}
-	
-	/*  top down:
-	 *  //global 
+
+	// top down:
+	// global
 	HashMap<Integer, Integer> hash = new HashMap<Integer, Integer>();
-	int numOfWays(int n) {
-		if(n == 0) return 1;
-		if (n == 1) return 1;
-		//	int val = hash.get(n-1) + hash.get(n-2);
-		//hash.add(n, val);
-		//Test if in the hash table → return from hash table
-		if (containsKey(n) ) {
+
+	int numOfWays2(int n) {
+		if (n == 0)
+			return 1;
+		if (n < 0)
+			return 0;
+
+		if (hash.containsKey(n)) {
 			return hash.get(n);
 		}
-		return hash.put (n,  numOfWays(n-1) + numOfWays(n - 2)); 
-	            // save back the hash table
-	}*/
+		return hash.put(n, numOfWays2(n - 1) + numOfWays2(n - 2));
 
+	}
 
 }
