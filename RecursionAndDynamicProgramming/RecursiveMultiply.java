094dd68589e11cac02b57f8c24b07ffb2a984a38
Jiaqi Chen <chen2007999@126.com>
2015-08-26 23:33:59 +0100

    RecursiveMultiply Algorithm

--- RecursionAndDynamicProgramming/RecursiveMultiply.java
+++ RecursionAndDynamicProgramming/RecursiveMultiply.java
@@ -20,7 +20,7 @@ public class RecursiveMultiply {
 		if (i == 0) {
 			return;
 		}
-		results[i + i] = results[i] + results[i];
+		results[i + 1] = results[i] + results[i];
 		recursiveIncrement(i - 1, results);
 	}
 
@@ -33,4 +33,24 @@ public class RecursiveMultiply {
 		return recursiveMultiply1Helper(newIndex, a, results);
 	}
 
+	int recursiveMultiply2(int a, int b) {
+		int bigger = a > b ? a : b;
+		int smaller = a > b ? b : a;
+		return recursiveMultiply2Helper(smaller, bigger);
+	}
+
+	private int recursiveMultiply2Helper(int smaller, int bigger) {
+		if (smaller == 0)
+			return 0;
+		if (smaller == 1) {
+			return bigger;
+		}
+		int s = smaller >> 1;
+		int half = recursiveMultiply2Helper(s, bigger);
+		if (smaller % 2 == 0) {
+			return half + half;
+		} else {
+			return half + half + bigger;
+		}
+	}
 }
