830d5b3b035b7840dd7a9b7fbfd3c40acb24de02
Jiaqi Chen <chen2007999@126.com>
2015-08-28 23:17:47 +0100

    Parens Algorithm Optimisation

--- RecursionAndDynamicProgramming/Parens.java
+++ RecursionAndDynamicProgramming/Parens.java
@@ -5,7 +5,7 @@ import java.util.Iterator;
 
 public class Parens {
 
-	HashSet<String> parens(int num) {
+	HashSet<String> parens1(int num) {
 		if (num < 0) {
 			return null;
 		}
@@ -18,7 +18,7 @@ public class Parens {
 			results.add("()");
 		}
 
-		HashSet<String> results = parens(num - 1);
+		HashSet<String> results = parens1(num - 1);
 		Iterator<String> it = results.iterator();
 		while (it.hasNext()) {
 			addNewParen(it.next(), results);
@@ -38,4 +38,36 @@ public class Parens {
 		}
 	}
 
+	HashSet<String> parens2(int num) {
+		if (num < 0)
+			return null;
+		HashSet<String> results = new HashSet<String>();
+		String s = "";
+		if (num == 0) {
+			results.add(s);
+			return results;
+		}
+		int toClose = 0;
+		dfs(num, toClose, s, results);
+		return results;
+	}
+
+	private void dfs(int num, int toClose, String s, HashSet<String> results) {
+		if (num == 0 && toClose == 0) {
+			results.add(s);
+			return;
+		}
+		int size = s.length();
+		if (num != 0) {
+			s += "(";
+			num--;
+			dfs(num, toClose, s, results);
+		}
+		s = s.substring(0, size);
+		if (toClose != 0) {
+			s += ")";
+			toClose--;
+			dfs(num, toClose, s, results);
+		}
+	}
 }
