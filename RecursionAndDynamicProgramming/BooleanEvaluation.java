fe01d9d20c3e53afa332142018b27254c6e20822
Jiaqi Chen <chen2007999@126.com>
2015-09-07 21:06:32 +0800

    BooleanEvalutation Algorithm

--- RecursionAndDynamicProgramming/BooleanEvaluation.java
+++ RecursionAndDynamicProgramming/BooleanEvaluation.java
@@ -1,5 +1,7 @@
 package RecursionAndDynamicProgramming;
 
+import java.util.HashMap;
+
 public class BooleanEvaluation {
 
 	/*
@@ -35,22 +37,25 @@ public class BooleanEvaluation {
 	 * }
 	 */
 
-	int booleanEvaluation(String s, boolean result) {
+	int booleanEvaluation(String s, boolean result, HashMap<String, Integer> memo) {
 		if (s.length() == 0) {
 			return 0;
 		}
 		if (s.length() == 1) {
 			return stringToBool(s) == result ? 1 : 0;
 		}
+		if (memo.containsKey(result + s)) {
+			return memo.get(result + s);
+		}
 		int ways = 0;
 		for (int i = 1; i < s.length(); i += 2) {
 			char c = s.charAt(i);
 			String left = s.substring(0, i);
 			String right = s.substring(i + 1, s.length());
-			int leftTrue = booleanEvaluation(left, true);
-			int leftFalse = booleanEvaluation(left, false);
-			int rightTrue = booleanEvaluation(right, true);
-			int rightFalse = booleanEvaluation(right, false);
+			int leftTrue = booleanEvaluation(left, true, memo);
+			int leftFalse = booleanEvaluation(left, false, memo);
+			int rightTrue = booleanEvaluation(right, true, memo);
+			int rightFalse = booleanEvaluation(right, false, memo);
 			int total = (leftTrue + leftFalse) * (rightTrue + rightFalse);
 
 			int totalTrue = 0;
@@ -65,6 +70,7 @@ public class BooleanEvaluation {
 			int subWays = result ? totalTrue : total - totalTrue;
 			ways += subWays;
 		}
+		memo.put(result + s, ways);
 		return ways;
 
 	}
