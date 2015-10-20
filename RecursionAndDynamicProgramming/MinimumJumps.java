22949d861223ba4a68cbdb11f582452efeeddf0f
Jiaqi Chen <chen2007999@126.com>
2015-10-20 21:24:20 +0100

    MinimumJumps Algorithm

--- RecursionAndDynamicProgramming/MinimumJumps.java
+++ RecursionAndDynamicProgramming/MinimumJumps.java
@@ -1,11 +1,13 @@
 package RecursionAndDynamicProgramming;
 
+import java.util.Hashtable;
+
 public class MinimumJumps {
 
 	public static void main(String[] args) {
 		int[] a = { 1, 1, 2, 0, 1, 2, 0 };
-		minimumJumps1(a);
-		System.out.println(result);
+		//minimumJumps1(a);
+		System.out.println(minimumJumps2(a));
 	}
 
 	static void minimumJumps1(int[] arr) {
@@ -26,6 +28,32 @@ public class MinimumJumps {
 			DFS(arr, i + jumps, count + 1);
 			jumps--;
 		}
+	}
+
+	static int minimumJumps2(int[] arr) {
+		Hashtable<Integer, Integer> hash = new Hashtable<>();
+		return DP(arr, arr.length, hash);
+
+	}
+
+	private static int DP(int[] arr, int index, Hashtable<Integer, Integer> hash) {
+		if (index == 0) {
+			return 0;
+		}
+		if (hash.containsKey(index)) {
+			return (int) hash.get(index);
+		}
+		int minJumps = Integer.MAX_VALUE;
+		for (int i = 0; i < index; i++) {
+			if (arr[i] + i >= index) {
+				int jumps = DP(arr, i, hash);
+				if (jumps < minJumps) {
+					minJumps = jumps;
+				}
+			}
+		}
+		hash.put(index, minJumps + 1);
+		return minJumps + 1;
 	}
 
 }
