4fc2a733f16e297d42addd8e0ab79c8a23e10a82
Jiaqi Chen <chen2007999@126.com>
2015-10-20 11:11:57 +0100

    Converstion Algorithm

--- BitsManipulation/Conversion.java
+++ BitsManipulation/Conversion.java
@@ -2,7 +2,7 @@ package BitsManipulation;
 
 public class Conversion {
 
-	int conversion(int n1, int n2) {
+	int conversion1(int n1, int n2) {
 		int result = 0;
 		while (n1 > 0 || n2 > 0) {
 			if (n1 % 2 != n2 % 2) {
@@ -14,4 +14,19 @@ public class Conversion {
 		return result;
 	}
 
+	int conversion2(int n1, int n2) {
+		int result = 0;
+		for (int n = n1 ^ n2; n > 0; n >>= 1) {
+			result += n & 1;
+		}
+		return result;
+	}
+
+	int conversion3(int n1, int n2) {
+		int result = 0;
+		for (int n = n1 ^ n2; n > 0; n = n & (n - 1)) {
+			result++;
+		}
+		return result;
+	}
 }
